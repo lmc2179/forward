9e00a88848bfe3df0c571a4a3dce60b76e33ad8a
lmc2179 <louiscialdella@gmail.com>
2016-11-09 23:09:39 -0800

    added zero case to backshift

--- forward/backshift_difference.py
+++ forward/backshift_difference.py
@@ -2,6 +2,8 @@ import numpy as np
 
 
 def backshift(data, positions=1):
+    if positions == 0:
+        return data
     backshifted_data = np.nan * np.empty(data.shape)
     backshifted_data[positions:] = data[:-positions]
     return backshifted_data
--- forward/tests/test_backshift_difference.py
+++ forward/tests/test_backshift_difference.py
@@ -15,6 +15,12 @@ class BackshiftTest(unittest.TestCase):
         A_shifted = [x if not np.isnan(x) else None for x in A_shifted]
         self.assertEqual(list(A_shifted), [None, None, 2])
 
+    def test_zero_backshift(self):
+        A = np.array([2, 4, 6])
+        A_shifted = backshift_difference.backshift(A, 0)
+        A_shifted = [x if not np.isnan(x) else None for x in A_shifted]
+        self.assertEqual(list(A_shifted), [2, 4, 6])
+
 class DifferenceTest(unittest.TestCase):
     def test_single_difference(self):
         A = np.array([2, 4, 6])
